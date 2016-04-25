79543081ff9a15e2ccdba60f717bcbaa3ae8f13b
Philipp Wollschlegel <folken@kabelsalat.ch>
2016-04-25 09:17:50 +0200

    Added "special" waypoints file

--- data/index.js
+++ data/index.js
@@ -2,6 +2,7 @@ exports.sections = [
     require('./maps.json'),
     require('./flarmnet.json'),
     require('./waypoints-by-country.json'),
+    require('./waypoints-special.json'),
     require('./airspace.json'),
     require('./openaip.json'),
     require('./gliding-hotspots.json'),
