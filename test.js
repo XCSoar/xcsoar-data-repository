711e2738006963501a26a230b75148c8afd83c66
elgandoz <elgandoz@gmail.com>
2021-12-10 23:52:57 +1100

    Test: check for valid filename and extension

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
+var path = require("path");
 var request = require("request");
-
 var repository = require("./");
 
 function describeSection(section) {
@@ -8,9 +8,44 @@ function describeSection(section) {
     });
 }
 
+function isValidFilename (fileName) {
+    var rgxForbiddenChars = /^[^\\/:\*\?"<>\|]+$/; // Forbidden characters \ / : * ? " < > |
+    var rgxNoLeadingDot = /^\./; // Cannot start with dot (.)
+    var rgxForbiddenFileNames = /^(nul|prn|con|aux|lpt[0-9]|com[0-9])(\.|$)/i; // Forbidden file names
+    return rgxForbiddenChars.test(fileName) && !rgxNoLeadingDot.test(fileName) && !rgxForbiddenFileNames.test(fileName);
+}
+
 function checkRecord(record) {
     it(record.name, function(done) {
         this.timeout(10000);
+
+        if (!isValidFilename(record.name)) {
+            throw new Error(record.name + " is an invalid filename");
+        }
+
+        var expectExt = null;
+        switch (record.type) {
+            case "map":
+                expectExt = "xcm";
+                break;
+            case "airspace":
+                expectExt = "txt";
+                break;
+            case "waypoint":
+                expectExt = "cup";
+                break;
+            case "waypoint-details":
+                expectExt = "txt";
+                break;
+            case "flarmnet":
+                expectExt = "fln";
+                break;
+            default:
+                break;
+        }
+        if (expectExt && path.extname(record.name).substr(1) !== expectExt) {
+            throw new Error(record.name + " has an invalid extension for its type (expected \"" + expectExt + "\")");
+        }
 
         var r = request.head(record.uri, function (error, response) {
             if (error && error.code !== "HPE_INVALID_CONSTANT") {
